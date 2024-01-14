f6d3c6066e036f262d51e06a602262267098f82e
Elchin <elcinceferli@gmail.com>
2024-01-14 04:06:47 -0800

    add headers

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,8 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -24,5 +26,6 @@ typedef struct binary_tree_s binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif
