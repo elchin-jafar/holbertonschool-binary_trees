fd7b5951705c4cf135badfb984e37dae2f71bcdc
root <muradabbaszade6@gmail.com>
2024-01-14 05:31:31 -0800

    Add depth function

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - get depth of the binary tree
+ * @tree: tree
+ * Return: depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+if (tree->parent != NULL)
+return (1 + binary_tree_depth(tree->parent);
+else
+return (0);
+}
