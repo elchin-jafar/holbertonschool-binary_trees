8905d5c540b750e9a834418c9e16ba570925979c
root <muradabbaszade6@gmail.com>
2024-01-14 05:38:12 -0800

    Add size function

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,7 +10,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 if (tree == NULL)
 return (0);
 if (tree->parent != NULL)
-  return (1 + binary_tree_depth(tree->parent));
+return (1 + binary_tree_depth(tree->parent));
 else
 return (0);
 }
---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - get size of the bt
+ * @tree: tree
+ * Return: size
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+	if (tree)
+	{
+		size ++;
+		size += binary_tree_size(tree->left);
+		size += binary_tree_size(tree->right);
+	}
+	return (size);
+}
